b7beeb597770c42c5c64c2aece9f6e546452bd7b
michal <michalczysz123@gmail.com>
2022-08-10 23:18:59 +0200

    Side bar added

--- papamobile/src/App.js
+++ papamobile/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import General from "./components/pages/general/general-page";
+import Detail from './components/pages/detail/detail-page';
 import Settings from "./components/pages/settings/settings-page";
 import About from "./components/pages/about/about-page"
+import TopBar from './components/TopBar/topbar-page';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+      
       <Router>
+        <TopBar />
         <Routes>
           <Route path="/">
             <Route index element={<General />} />
+            <Route path="detail" element={<Detail />} />
             <Route path="settings" element={<Settings />} />
             <Route path="about" element={<About />} />
           </Route>
--- papamobile/src/components/pages/general/general-page.jsx
+++ papamobile/src/components/pages/general/general-page.jsx
@@ -1,31 +1,7 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton } from '@mui/material'
-import { Menu as MenuIcon } from "@mui/icons-material";
-import "./general-page.css"
-import SearchForm from '../../search/search-component'
 
-function General() {
-  return (
-    <div className="App">
-      <AppBar className="App-header" position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <h2 className='title'>papimobile</h2>
-        </Toolbar>
-      </AppBar>
-      <div className='body'>
-        <SearchForm />
-      </div>
-    </div>
-  );
+function General () {
+    
 }
 
-export default General;
+export default General
