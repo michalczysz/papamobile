36e7b5d29254264c922304f7a8743bddeec70ecd
michalczysz <DLXP7EM@emea.vwg>
2022-08-11 11:06:38 +0200

    test plotly

--- papamobile/src/App.js
+++ papamobile/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import General from "./components/pages/general/general-page";
-import Detail from './components/pages/detail/detail-page';
-import Settings from "./components/pages/settings/settings-page";
-import About from "./components/pages/about/about-page"
-import TopBar from './components/TopBar/topbar-page';
+import React, {useState} from 'react'
+// import General from "./components/pages/general/general-page";
+// import Detail from './components/pages/detail/detail-page';
+// import Settings from "./components/pages/settings/settings-page";
+// import About from "./components/pages/about/about-page"
+// import TopBar from './components/TopBar/topbar-page';
+import Test from "./components/pages/test/test-page"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [state, setState] = useState("loading")
   return (
 
     <div className="App">
-      
       <Router>
-        <TopBar />
+        {/* <TopBar /> */}
         <Routes>
           <Route path="/">
-            <Route index element={<General />} />
+            <Route path="test" element={<Test state={state} setState={setState}/>} />
+            {/* <Route index element={<General />} />
             <Route path="detail" element={<Detail />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="about" element={<About />} />
+            <Route path="about" element={<About />} /> */}
           </Route>
         </Routes>
       </Router>
