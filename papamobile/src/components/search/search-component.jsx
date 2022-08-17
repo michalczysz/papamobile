8075da4dc86a153dd03d9e154026433c7b57ac9c
michal <michalczysz123@gmail.com>
2022-08-17 22:42:42 +0200

    general plots polishing

--- papamobile/src/components/search/search-component.jsx
+++ papamobile/src/components/search/search-component.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import axios from 'axios';
+import useEffectOnce from '../../ReactEX'
+
 import { Grid, InputLabel, FormControl, Select, MenuItem, Button } from '@mui/material'
 import { Card, CardContent } from '@mui/material'
 import "./search-component.css"
@@ -9,28 +12,84 @@ function SearchForm(e) {
         console.log('lol')
     }
 
+    const [state, setState] = React.useState({ 'Brand': '', 'Model': '', 'Year': '', 'Fuel': '' });
+    const [brands, setBrands] = React.useState([])
+    const [models, setModels] = React.useState([])
+
+    const handleChangeBrand = (event, param) => {
+        setState({ ...state, [param]: event.target.value });
+
+        axios.get('http://34.141.144.103:8000/base/mbrand')
+            .then((response) => {
+                // console.log(response)
+                let temp = []
+                response.data.forEach(brand_r => {
+                    temp.push([brand_r.brand, brand_r.count])
+                })
+                setBrands(temp)
+            })
+    };
+
+    useEffectOnce(() => {
+        axios.get('http://34.141.144.103:8000/base/mbrand')
+            .then((response) => {
+                // console.log(response)
+                let temp = []
+                response.data.forEach(brand_r => {
+                    temp.push([brand_r.brand, brand_r.count])
+                })
+                setBrands(temp)
+            })
+    })
+
     return (<>
         <div className="grid-container">
             <Card sx={{ width: '100%' }}>
-                <label className="form-title">Czego szukasz?</label>
+                <label className="form-title">What are you looking for?</label>
                 <CardContent>
                     <Grid container spacing={{ xs: 3 }} columns={{ xs: 6, sm: 12 }}>
-                        {['Marka', 'Model', 'Rok', 'Paliwo'].map((e, index) => (
-                            <Grid item xs={6} sm={6} key={e + index}>
-                                <FormControl fullWidth>
-                                    <InputLabel id="demo-simple-select-label">{e}</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        id="demo-simple-select"
-                                    >
-                                        <MenuItem>Ten</MenuItem>
-                                        <MenuItem>Twenty</MenuItem>
-                                        <MenuItem>Thirty</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </Grid>
-                        ))}
-                        <Grid item xs={12} key={"button_search"}><Button style={{width: '100%'}} onClick={onClickMethod} variant="contained">Szukaj</Button></Grid>
+                        {/* Brand Select */}
+                        <Grid item xs={6} sm={6}>
+                            <FormControl fullWidth>
+                                <InputLabel>Brand</InputLabel>
+                                <Select
+                                    value={state.Brand}
+                                    label={'Brand'}
+                                    onChange={event => handleChangeBrand(event, 'Brand')}
+                                    MenuProps={{
+                                        style: {
+                                            maxHeight: 400,
+                                        },
+                                    }}>
+                                    {brands.map(brand => {
+                                        return <MenuItem value={brand[0]} key={'key' + brand[0]}>{brand[0] + ' (' + brand[1] + ')'}</MenuItem>
+                                    })}
+                                    {/* <MenuItem value={10}>Ten</MenuItem> */}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        {/* Model Select */}
+                        <Grid item xs={6} sm={6}>
+                            <FormControl fullWidth>
+                                <InputLabel>Model</InputLabel>
+                                <Select
+                                    value={state.Model}
+                                    label={'Model'}
+                                    onChange={event => handleChangeModel(event, 'Model')}
+                                    MenuProps={{
+                                        style: {
+                                            maxHeight: 400,
+                                        },
+                                    }}>
+                                    {models.map(model => {
+                                        return <MenuItem value={model[0]} key={'key' + model[0]}>{model[0] + ' (' + model[1] + ')'}</MenuItem>
+                                    })}
+                                    {/* <MenuItem value={10}>Ten</MenuItem> */}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        {/* Year select */}
+                        <Grid item xs={12} key={"button_search"}><Button style={{ width: '100%' }} onClick={onClickMethod} variant="contained">Search</Button></Grid>
                     </Grid>
                 </CardContent>
             </Card>
