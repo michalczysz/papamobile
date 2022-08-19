f18babf32b232cda9483fcb00f6e6eaebe2d3387
michalczysz <michalczysz123@gmail.com>
2022-08-19 17:01:54 +0000

    about mobile fix

--- papamobile/src/components/pages/about/about-page.jsx
+++ papamobile/src/components/pages/about/about-page.jsx
@@ -14,55 +14,62 @@ function About() {
   };
 
   return (
-    <div className='about'>
-      <div className="box">
-        <img src={reactLogo} alt="react logo" width={250} height={250}/>
-        <img src={muiLogo} alt="mui logo" width={250} height={250}/>
-      </div>
-      <div className="box2">
-        <img src={plotlytLogo} alt="plotly logo" />
+    <>
+      <div className='about'>
+        <div className="row">
+          <div className="box"><img src={reactLogo} alt="react logo" /></div>
+          <div className="box"><img src={muiLogo} alt="mui logo" /></div>
+        </div>
+        <div className="row box"><img src={plotlytLogo} alt="plotly logo" /></div>
       </div>
       <div className='info'>
-        <Grid container spacing={{ xs: 3 }} columns={{ xs: 6, sm: 12 }}>
-          <Grid item xs={6}>
+        <Grid container spacing={{ xs: 3 }} columns={{ xs: 1, sm: 2 }}>
+          <Grid item xs={1}>
             <div className='authors'>
               <div className="info-title"><b>Authors</b></div>
-              <div onClick={() => openInNewTab('https://github.com/michalczysz')} className='author'>
-                <GHIcon /> Web backend/frontend: Michał Czysz
-            </div>
-              <div onClick={() => openInNewTab('https://github.com/ignacyr')} className='author'>
-                <GHIcon /> Web scrapping software: Ignacy Rogatty
-            </div>
+              <div style={{ "margin": "auto" }}>
+                <div onClick={() => openInNewTab('https://github.com/michalczysz')} className='author'>
+                  <GHIcon /> Web backend/frontend: Michał Czysz
+                </div>
+                <div onClick={() => openInNewTab('https://github.com/ignacyr')} className='author'>
+                  <GHIcon /> Web scrapping software: Ignacy Rogatty
+                </div>
+              </div>
             </div>
           </Grid>
-          <Grid item xs={6}>
-            <div className='technologies'>
+          <Grid item xs={1}>
+            <div className='technologies authors'>
               <div className="info-title"><b>Stack</b></div>
-              Frontend (JavaScript):
-          <br />
-              <li>React</li>
-              <li>Material UI</li>
-              <li>Plotly</li>
-              <br />
-              Backend (Python):
-          <br />
-              <li>Django</li>
-              <li>Django Rest Framework</li>
-              <br />
-              Web Scraper (Python):
-          <br />
-              <li>Scrapy - library</li>
-              <li>Pandas</li>
-              <br />
-              Databases:
-          <br />
-              <li>SQLite3</li>
-              <li>Amazon S3</li>
+              <div style={{ "margin": "auto" }}>
+                <div>
+                  Frontend (JavaScript):
+                  <br />
+                  <li>React</li>
+                  <li>Material UI</li>
+                  <li>Plotly</li>
+                  <br />
+                  Backend (Python):
+                  <br />
+                  <li>Django</li>
+                  <li>Django Rest Framework</li>
+                  <br />
+                  Web Scraper (Python):
+                  <br />
+                  <li>Scrapy - library</li>
+                  <li>Pandas</li>
+                  <br />
+                  Databases:
+                  <br />
+                  <li>SQLite3</li>
+                  <li>Amazon S3</li>
+                </div>
+
+              </div>
             </div>
           </Grid>
         </Grid>
       </div>
-    </div>
+    </>
   )
 }
 
